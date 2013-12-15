b6190605a3367ef4eaaa3f6100e773c1c9d7c24d
calvin <guofuchunlh@gmail.com>
2013-12-15 14:04:29 +0800

    add topics seed

--- app/database/seeds/TopicsTableSeeder.php
+++ app/database/seeds/TopicsTableSeeder.php
@@ -8,11 +8,28 @@ class TopicsTableSeeder extends Seeder {
 		// DB::table('topics')->truncate();
 
 		$topics = array(
-
+			array(
+				'title' => '第一篇帖子', 'body' => '第一篇帖子的内容','reply_at' => time(),
+			),
+			array(
+				'title' => '第二篇帖子', 'body' => '第二篇帖子的内容','reply_at' => time(),
+			),
+			array(
+				'title' => '第三篇帖子', 'body' => '第三篇帖子的内容','reply_at' => time(),
+			),
+			array(
+				'title' => '第四篇帖子', 'body' => '第四篇帖子的内容','reply_at' => time(),
+			),
+			array(
+				'title' => '第五篇帖子', 'body' => '第五篇帖子的内容','reply_at' => time(),
+			),
+			array(
+				'title' => '第六篇帖子', 'body' => '第六篇帖子的内容','reply_at' => time(),
+			),
 		);
 
 		// Uncomment the below to run the seeder
-		// DB::table('topics')->insert($topics);
+		DB::table('topics')->insert($topics);
 	}
 
 }
